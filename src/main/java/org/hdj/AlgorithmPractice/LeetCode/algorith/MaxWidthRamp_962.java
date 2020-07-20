cecf182a67abd23c5f18e9b3d6694330c258791a
h-dj <1432517356@qq.com>
2020-07-20 23:42:51 +0800

    1. 搜索循环数组

---
+++ src/main/java/org/hdj/AlgorithmPractice/LeetCode/algorith/MaxWidthRamp_962.java
@@ -0,0 +1,89 @@
+package org.hdj.AlgorithmPractice.LeetCode.algorith;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * @author hdj
+ * @version 1.0
+ * @date 09/06/2020 21:47
+ * @description: 给定一个整数数组 A，坡是元组 (i, j)，其中  i < j 且 A[i] <= A[j]。这样的坡的宽度为 j - i。
+ * <p>
+ * 找出 A 中的坡的最大宽度，如果不存在，返回 0 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：[6,0,8,2,1,5]
+ * 输出：4
+ * 解释：
+ * 最大宽度的坡为 (i, j) = (1, 5): A[1] = 0 且 A[5] = 5.
+ * 示例 2：
+ * <p>
+ * 输入：[9,8,1,0,1,9,4,0,4,1]
+ * 输出：7
+ * 解释：
+ * 最大宽度的坡为 (i, j) = (2, 9): A[2] = 1 且 A[9] = 1.
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 2 <= A.length <= 50000
+ * 0 <= A[i] <= 50000
+ */
+public class MaxWidthRamp_962 {
+
+    public static void main(String[] args) {
+        System.out.println(maxWidthRamp(new int[]{6, 0, 8, 2, 1, 5, 56, 86, 4, 3}));
+        System.out.println(maxWidthRamp2(new int[]{2, 2, 1}));
+    }
+
+    /**
+     * 暴力
+     *
+     * @param A
+     * @return
+     */
+    public static int maxWidthRamp(int[] A) {
+        if (A == null || A.length == 0) {
+            return 0;
+        }
+        int max = 0;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A.length; j++) {
+                if (i < j && A[i] <= A[j]) {
+                    if (max < (j - i)) {
+                        max = j - i;
+                    }
+                }
+            }
+        }
+        return max;
+    }
+
+
+    /**
+     * 排序
+     *
+     * @param A
+     * @return
+     */
+    public static int maxWidthRamp2(int[] A) {
+        int N = A.length;
+        Integer[] B = new Integer[N];
+        for (int i = 0; i < A.length; i++) {
+            B[i] = i;
+        }
+        Arrays.sort(B, (i, j) -> ((Integer) A[i]).compareTo(A[j]));
+
+        int ans = 0;
+        int m = N;
+        for (int i : B) {
+            ans = Math.max(ans, i - m);
+            m = Math.min(m, i);
+        }
+
+        return ans;
+    }
+}
---
+++ src/main/java/org/hdj/AlgorithmPractice/LeetCode/algorith/SearchRotateArray_1003.java
@@ -0,0 +1,92 @@
+package org.hdj.AlgorithmPractice.LeetCode.algorith;
+
+/**
+ * @author hdj
+ * @version 1.0
+ * @date 2020/7/20 下午10:27
+ * @description: 面试题 10.03. 搜索旋转数组
+ * <p>
+ * <p>
+ * 搜索旋转数组。给定一个排序后的数组，包含n个整数，但这个数组已被旋转过很多次了，次数不详。请编写代码找出数组中的某个元素，假设数组元素原先是按升序排列的。若有多个相同元素，返回索引值最小的一个。
+ * <p>
+ * 示例1:
+ * <p>
+ * 输入: arr = [15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14], target = 5
+ * 输出: 8（元素5在该数组中的索引）
+ * 示例2:
+ * <p>
+ * 输入：arr = [15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14], target = 11
+ * 输出：-1 （没有找到）
+ * 提示:
+ * <p>
+ * arr 长度范围在[1, 1000000]之间
+ */
+public class SearchRotateArray_1003 {
+
+    public static void main(String[] args) {
+        int[] arr = {15,4,5, 20, 25, 1, 3, 16, 19,10};
+        System.out.println(search_1(arr, 5));
+
+        System.out.println(search_2(arr, 5));
+    }
+
+
+    /**
+     * 暴力美学
+     *
+     * @param arr
+     * @return
+     */
+    public static int search_1(int[] arr, int target) {
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 二分查找
+     * https://leetcode-cn.com/problems/search-rotate-array-lcci/solution/er-fen-fa-by-armeria-program/
+     *
+     * @param arr
+     * @param target
+     * @return
+     */
+    public static int search_2(int[] arr, int target) {
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start >> 1);
+
+            if (arr[mid] >= arr[start]) {
+                if (arr[start] <= target && arr[mid] >= target) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else if (arr[mid] < arr[start]) {
+                if (arr[start] <= target || arr[mid] >= target) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else if (arr[mid] == arr[start]) {
+                if (arr[start] != target) {
+                    start++;
+                } else {
+                    return start;
+                }
+            }
+        }
+        return -1;
+    }
+}
