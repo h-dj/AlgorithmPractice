2a5079a175f3e9cce0708224c2343d369952705c
h-dj <1432517356@qq.com>
2021-08-26 15:32:52 +0800

    fix: two sum

--- src/main/java/org/hdj/AlgorithmPractice/LeetCode/algorithm/TwoSumAll.java
+++ src/main/java/org/hdj/AlgorithmPractice/LeetCode/algorithm/TwoSumAll.java
@@ -6,30 +6,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @author hdj
- * @version 1.0
- * @date 2019/9/24 23:15
- * @description: 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
- * <p>
- * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
- * <p>
- * 示例:
- * <p>
- * 给定 nums = [2, 7, 11, 15], target = 9
- * <p>
- * 因为 nums[0] + nums[1] = 2 + 7 = 9
- * 所以返回 [0, 1]
+ * @Author huangjiajian
+ * @Date 2021/8/26 14:54
+ * @Description: 重复无序数组找出相加为target的所有对(两数之和 的变种)
  */
-public class TwoSum {
+public class TwoSumAll {
 
-    private static int[] nums = {2, 7, 11, 15};
+    private static int[] nums = {2, 7, 11, 8, 15, 1, 6, 3};
     private static int target = 9;
 
     public static void main(String[] args) {
-        int[] ints = twoSum1(nums, target);
-
-        ints = twoSum2(nums, target);
-        System.out.println(Arrays.toString(ints));
+        int[] ints = twoSum2(nums, target);
+        for(int i = 0; i < ints.length; i++) {
+          System.out.print(ints[i]);
+        }
+        System.out.println();
     }
 
     /**
@@ -42,14 +33,19 @@ public class TwoSum {
      * @return
      */
     public static int[] twoSum1(int[] nums, int target) {
+        int index = 0;
+        int[] result = new int[nums.length];
+        Arrays.fill(result,-1);
+
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (target - nums[j] == nums[i]) {
-                    return new int[]{i, j};
+                    result[index++] = i;
+                    result[index++] = j;
                 }
             }
         }
-        throw new IllegalArgumentException();
+        return result;
     }
 
     /**
@@ -62,15 +58,21 @@ public class TwoSum {
      * @return
      */
     public static int[] twoSum2(int[] nums, int target) {
+        int index = 0;
+        int[] result = new int[nums.length];
+        Arrays.fill(result,-1);
+
         Map<Integer, Integer> map = new HashMap<>(nums.length);
         for (int i = 0; i < nums.length; i++) {
             int key = target - nums[i];
             if (map.containsKey(key)) {
-                return new int[]{map.get(key), i};
+                result[index++] = map.get(key);
+                result[index++] = i;
             }
             map.put(nums[i], i);
         }
-        throw new IllegalArgumentException();
+        return result;
     }
+
 
 }
